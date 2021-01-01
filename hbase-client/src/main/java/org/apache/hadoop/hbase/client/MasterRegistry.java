dbaa515443b4fd5b42a8b07dc18b92dae869fe39
Duo Zhang <zhangduo@apache.org>
2021-01-01 22:55:03 +0800

    HBASE-26128 Backport HBASE-25454 "Add trace support for connection registry" to branch-2
    5/17 commits of HBASE-22120, original commit dcb78bd4bda4a4ae13d863df8aec266031e5bc93

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/MasterRegistry.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/MasterRegistry.java
@@ -18,6 +18,8 @@
 package org.apache.hadoop.hbase.client;
 
 import static org.apache.hadoop.hbase.HConstants.MASTER_ADDRS_KEY;
+import static org.apache.hadoop.hbase.trace.TraceUtil.trace;
+import static org.apache.hadoop.hbase.trace.TraceUtil.tracedFuture;
 import static org.apache.hadoop.hbase.util.DNS.getHostname;
 import static org.apache.hadoop.hbase.util.FutureUtils.addListener;
 
@@ -264,18 +266,23 @@ public class MasterRegistry implements ConnectionRegistry {
 
   @Override
   public CompletableFuture<RegionLocations> getMetaRegionLocations() {
-    return this.<GetMetaRegionLocationsResponse> call((c, s, d) -> s.getMetaRegionLocations(c,
-      GetMetaRegionLocationsRequest.getDefaultInstance(), d), r -> r.getMetaLocationsCount() != 0,
-      "getMetaLocationsCount").thenApply(MasterRegistry::transformMetaRegionLocations);
+    return tracedFuture(
+      () -> this
+        .<GetMetaRegionLocationsResponse> call(
+          (c, s, d) -> s.getMetaRegionLocations(c,
+            GetMetaRegionLocationsRequest.getDefaultInstance(), d),
+          r -> r.getMetaLocationsCount() != 0, "getMetaLocationsCount")
+        .thenApply(MasterRegistry::transformMetaRegionLocations),
+      "MasterRegistry.getMetaRegionLocations");
   }
 
   @Override
   public CompletableFuture<String> getClusterId() {
-    return this
+    return tracedFuture(() -> this
       .<GetClusterIdResponse> call(
         (c, s, d) -> s.getClusterId(c, GetClusterIdRequest.getDefaultInstance(), d),
         GetClusterIdResponse::hasClusterId, "getClusterId()")
-      .thenApply(GetClusterIdResponse::getClusterId);
+      .thenApply(GetClusterIdResponse::getClusterId), "MasterRegistry.getClusterId");
   }
 
   private static boolean hasActiveMaster(GetMastersResponse resp) {
@@ -298,21 +305,23 @@ public class MasterRegistry implements ConnectionRegistry {
 
   @Override
   public CompletableFuture<ServerName> getActiveMaster() {
-    CompletableFuture<ServerName> future = new CompletableFuture<>();
-    addListener(call((c, s, d) -> s.getMasters(c, GetMastersRequest.getDefaultInstance(), d),
-      MasterRegistry::hasActiveMaster, "getMasters()"), (resp, ex) -> {
-        if (ex != null) {
-          future.completeExceptionally(ex);
-        }
-        ServerName result = null;
-        try {
-          result = filterActiveMaster((GetMastersResponse)resp);
-        } catch (IOException e) {
-          future.completeExceptionally(e);
-        }
-        future.complete(result);
-      });
-    return future;
+    return tracedFuture(() -> {
+      CompletableFuture<ServerName> future = new CompletableFuture<>();
+      addListener(call((c, s, d) -> s.getMasters(c, GetMastersRequest.getDefaultInstance(), d),
+        MasterRegistry::hasActiveMaster, "getMasters()"), (resp, ex) -> {
+          if (ex != null) {
+            future.completeExceptionally(ex);
+          }
+          ServerName result = null;
+          try {
+            result = filterActiveMaster((GetMastersResponse) resp);
+          } catch (IOException e) {
+            future.completeExceptionally(e);
+          }
+          future.complete(result);
+        });
+      return future;
+    }, "MasterRegistry.getActiveMaster");
   }
 
   private static List<ServerName> transformServerNames(GetMastersResponse resp) {
@@ -333,11 +342,13 @@ public class MasterRegistry implements ConnectionRegistry {
 
   @Override
   public void close() {
-    if (masterAddressRefresher != null) {
-      masterAddressRefresher.close();
-    }
-    if (rpcClient != null) {
-      rpcClient.close();
-    }
+    trace(() -> {
+      if (masterAddressRefresher != null) {
+        masterAddressRefresher.close();
+      }
+      if (rpcClient != null) {
+        rpcClient.close();
+      }
+    }, "MasterRegistry.close");
   }
 }
